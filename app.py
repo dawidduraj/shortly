68174c1061c92d86702f1c0794bfebbe0feecafb
Dawid Duraj <dawid.duraj@outlook.de>
2022-06-16 01:31:02 +0200

    Finish Expand Page

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def shorten():
     return render_template("index.html")
+
+@app.route("/expand")
+def expand():
+    return render_template("expand.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
