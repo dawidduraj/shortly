e39a331898a549963ec842aaddd8bdd25b26da80
Dawid Duraj <dawid.duraj@outlook.de>
2022-06-19 02:00:55 +0200

    Create Database

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, flash, render_template, request
+from importlib.resources import path
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.String(200), nullable=False)
+    path = db.Column(db.String(6), nullable=False)
+
+
+@app.route("/", methods=["POST","GET"])
 def shorten():
+
+    #URL validate
+    url = request.form["url"]
+    if("www." in url):
+        url = f'https://{url}/'
+        print(url)
+    elif ("https://" not in url):
+        url = f'https://www.{url}/'
+    
     return render_template("index.html")
 
 @app.route("/expand", methods=["POST","GET"])
